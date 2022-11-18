9805a3cdd59a56dd305e562ff18302af2e258b0e
Hang Le <hang.le01@student.csulb.edu>
2022-11-17 16:29:02 -0800

    last

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ app.get("/", (req, res) => {
 // Server configuration
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: false })); // <--- middleware configuration
 
 
 // Add the /about path
@@ -95,3 +96,60 @@ app.get("/books", (req, res) => {
     res.render("books", { model: result.rows });
   });
 });
+
+// GET /edit/5
+app.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Books WHERE Book_ID = $1";
+  pool.query(sql, [id], (err, result) => {
+    // if (err) ...
+    res.render("edit", { model: result.rows[0] });
+  });
+});
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const book = [req.body.title, req.body.author, req.body.comments, id];
+  const sql = "UPDATE Books SET Title = $1, Author = $2, Comments = $3 WHERE (Book_ID = $4)";
+  pool.query(sql, book, (err, result) => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+// GET /create
+app.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
+
+// POST /create
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO Books (Title, Author, Comments) VALUES ($1, $2, $3)";
+  const book = [req.body.title, req.body.author, req.body.comments];
+  pool.query(sql, book, (err, result) => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+// GET /delete/5
+app.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Books WHERE Book_ID = $1";
+  pool.query(sql, [id], (err, result) => {
+    // if (err) ...
+    res.render("delete", { model: result.rows[0] });
+  });
+});
+
+// POST /delete/5
+app.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM Books WHERE Book_ID = $1";
+  pool.query(sql, [id], (err, result) => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
